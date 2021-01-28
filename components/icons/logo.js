370c4b9fbd0cae8ac54429bf9a059c875a7d5db3
zphw <zph@zph.io>
2021-01-28 14:49:55 +0800

    fix: icon path

--- components/icons/logo.js
+++ components/icons/logo.js
@@ -1,6 +1,6 @@
 
 const Logo = ({ className = ``, hidden = true }) => (
-    <img className={className} alt="Van Gogh icon" src="https://blog.zph.io/images/favicon.png" lazy="loaded" />
+    <img className={className} alt="Logo icon" src="/favicon.png" lazy="loaded" />
 )
 
 export default Logo
